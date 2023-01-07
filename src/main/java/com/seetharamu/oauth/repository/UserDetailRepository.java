59a63e8ca94cbc2b62322c8de8915b80e2d4d80f
seetharamugn <seetharamu@gmail.com>
2023-01-08 01:07:15 +0530

    created model repo service packages some pre declaration

---
+++ src/main/java/com/seetharamu/oauth/repository/UserDetailRepository.java
@@ -0,0 +1,10 @@
+package com.seetharamu.oauth.repository;
+
+import com.seetharamu.oauth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserDetailRepository extends JpaRepository<User, Integer> {
+
+}
